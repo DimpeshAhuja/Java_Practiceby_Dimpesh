359e210d7cf9fc754826d9baf72b81597ab3451d
dimpeshahuja <dimpeshahuja21@gmail.com>
2024-09-30 12:08:34 +0530

    This is a Readme file

---
+++ Primitive Datatype/InitializeLiterals.java
@@ -0,0 +1,16 @@
+public class InitializeLiterals {
+    
+    public static void main(String[] args) {
+        int i = 1;
+        int hexaDec = 0x1d;
+        int biDec = 0b101;
+        int octDec = 011;
+ 
+        System.out.println(i);
+        System.out.println(hexaDec);
+        System.out.println(biDec);
+        System.out.println(octDec);
+    }
+    
+
+}
---
+++ Primitive Datatype/Underscore.java
@@ -0,0 +1,10 @@
+public class Underscore {
+    public static void main(String[] args) {
+        
+        byte num = 0b11_10;
+        int i = 55_454;
+
+        System.out.println(num);
+        System.out.println(i);
+    }
+}
